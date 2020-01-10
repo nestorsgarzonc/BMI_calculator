9e991094f4486fc4fb6b6f96f633a20db0400fc7
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-01-10 18:51:38 -0500

    refactoring code

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -15,11 +15,43 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI Calculator'),
         centerTitle: true,
       ),
-      body: Center(child: Text('Calculator')),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(child: BuildContainer(colour: Color(0xFF1D1E33))),
+                Expanded(child: BuildContainer(colour: Color(0xFF1D1E33))),
+              ],
+            ),
+          ),
+          Expanded(child: BuildContainer(colour: Color(0xFF1D1E33))),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(child: BuildContainer(colour: Color(0xFF1D1E33))),
+                Expanded(child: BuildContainer(colour: Color(0xFF1D1E33))),
+              ],
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class BuildContainer extends StatelessWidget {
+  final Color colour;
+  BuildContainer({@required this.colour});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: colour,
+      ),
+      child: Container(),
     );
   }
 }
